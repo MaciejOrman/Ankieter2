8e8c91fec1c3ab09e76ef7a99a26ac36678ccffc
Maciek <orman.maciej@gmail.com>
2015-02-08 18:30:52 +0100

    Konto++

---
+++ src/main/java/pl/wroc/pwr/data/models/Konto.java
@@ -0,0 +1,21 @@
+package pl.wroc.pwr.data.models;
+
+public class Konto {
+
+	String login;
+	String haslo;
+	
+	public String getHaslo() {
+		return haslo;
+	}
+	public void setHaslo(String haslo) {
+		this.haslo = haslo;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+}
